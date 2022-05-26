7f3106463987d3615b70a0816530733a2e001323
Yuji Nakamura <ny5117@icloud.com>
2022-05-26 20:36:46 +0900

    不要なimportを削除

--- src/components/HabitItem.tsx
+++ src/components/HabitItem.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import styled from "styled-components";
 import { Habit } from "../typings";
-import HabitTitle from "./HabitTitle";
 
 interface PropsType {
   habit: Habit;
